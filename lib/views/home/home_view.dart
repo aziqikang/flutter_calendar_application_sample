baa1ef56c9106fd75f795db0ec95ee481ca5c388
Amy Kang <aziqikang@gmail.com>
2024-01-20 12:42:47 -0500

    Embed Calendar

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:googleapis/calendar/v3.dart' as GoogleAPI;
 import 'package:http/io_client.dart' show IOClient, IOStreamedResponse;
 import 'package:http/http.dart' show BaseRequest, Response;
 import 'package:syncfusion_flutter_calendar/calendar.dart';
